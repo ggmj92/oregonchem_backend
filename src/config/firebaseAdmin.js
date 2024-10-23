8e77c01b1476855c7d135b48047ff988cecdd455
Gabriel Garcia-Miro Jeri <ggmj92@gmail.com>
2024-10-23 12:06:48 -0500

    environment variables changed

--- src/config/firebaseAdmin.js
+++ src/config/firebaseAdmin.js
@@ -1,8 +1,6 @@
 const admin = require('firebase-admin');
-const fs = require('fs');
-const path = require('path');
 
-const serviceAccount = JSON.parse(fs.readFileSync(path.join(__dirname, '../utils/serviceAccountKey.json'), 'utf8'));
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 if (!admin.apps.length) {
     admin.initializeApp({
