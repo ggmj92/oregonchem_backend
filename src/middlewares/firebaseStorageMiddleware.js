beaaf1febc455c54e507850aada6450712c53ddb
Gabriel Garcia-Miro Jeri <ggmj92@gmail.com>
2024-10-23 15:53:16 -0500

    multer fixes

--- src/middlewares/firebaseStorageMiddleware.js
+++ src/middlewares/firebaseStorageMiddleware.js
@@ -154,8 +154,14 @@ const firebaseStorageMiddleware = async (req, res, next) => {
 const upload = multer({
     storage: multerStorage,
     limits: {
-        fileSize: 1024 * 1024 * 3,
+        fileSize: 1024 * 1024 * 3, // 3 MB limit
     },
-});
+}).fields([
+    { name: 'images[site1]', maxCount: 1 },
+    { name: 'images[site2]', maxCount: 1 },
+    { name: 'images[site3]', maxCount: 1 },
+    { name: 'images[site4]', maxCount: 1 },
+    { name: 'images[site5]', maxCount: 1 },
+]);
 
 module.exports = { upload, firebaseStorageMiddleware };
