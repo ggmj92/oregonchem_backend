3c4d9891beb18e7d5644cdf2708203e3be3731b9
Gabriel Garcia-Miro Jeri <ggmj92@gmail.com>
2024-10-23 13:00:16 -0500

    changes made on productcontroller

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -3,32 +3,30 @@ const Product = require("../models/Product");
 const ProductController = {
     // GET ALL PRODUCTS
     async getAllProducts(req, res) {
-
         try {
             const { site } = req.query;
-
             const products = await Product.find()
                 .populate("presentations categories")
                 .exec();
             console.log('Products fetched:', products);
-
-
             const filteredProducts = products.map((product) => {
-                let siteSpecificData = product.siteData;
-
+                let siteSpecificData = {
+                    descriptions: product.descriptions[site],
+                    uses: product.uses[site],
+                    images: product.images[site]
+                };
                 if (site) {
-                    const specificSiteData = product.siteData.find((data) => data.site === site);
-                    siteSpecificData = specificSiteData ? [specificSiteData] : [];
+                    const specificSiteData = product.descriptions[site] ? siteSpecificData : {};
                 }
-
                 return {
                     name: product.name,
                     presentations: product.presentations,
                     categories: product.categories,
-                    siteData: siteSpecificData,
+                    descriptions: specificSiteData.descriptions,
+                    uses: specificSiteData.uses,
+                    images: specificSiteData.images
                 };
             });
-
             res.status(200).json(filteredProducts);
         } catch (error) {
             res.status(500).json({ message: "Error fetching products", error });
@@ -39,21 +37,11 @@ const ProductController = {
     async createProduct(req, res) {
         try {
             const { name, presentations, categories, descriptions, uses } = req.body;
-            const siteData = [];
-
+            const images = {};
             ["site1", "site2", "site3", "site4", "site5"].forEach((site, index) => {
-                const description = descriptions[index];
-                const use = uses[index];
                 const imageFile = req.files[`site${index + 1}`];
-
-                if (description || use || (imageFile && imageFile.length > 0)) {
-                    siteData.push({
-                        site: `site${index + 1}`,
-                        descriptions: description || "",
-                        uses: use || "",
-                        images:
-                            imageFile && imageFile.length > 0 ? imageFile[0].downloadURL : "",
-                    });
+                if (imageFile && imageFile.length > 0) {
+                    images[site] = imageFile[0].downloadURL;
                 }
             });
 
@@ -61,7 +49,9 @@ const ProductController = {
                 name,
                 presentations: JSON.parse(presentations),
                 categories: JSON.parse(categories),
-                siteData,
+                descriptions,
+                uses,
+                images
             });
 
             const savedProduct = await newProduct.save();
@@ -69,42 +59,6 @@ const ProductController = {
         } catch (error) {
             console.error("Error creating product:", error);
             res.status(500).json({ message: "Error creating product", error });
-        }
-    },
-
-    getProductByIdAndSite: async (req, res) => {
-        const { id, site } = req.params;
-
-        try {
-            const product = await Product.findById(id)
-                .populate("presentations categories")
-                .exec();
-
-            if (!product) {
-                return res.status(404).json({ message: "Product not found" });
-            }
-
-            const siteData = product.siteData.find((data) => data.site === site);
-
-            if (!siteData) {
-                return res
-                    .status(404)
-                    .json({ message: "No data for the requested site" });
-            }
-
-            const response = {
-                name: product.name,
-                presentations: product.presentations,
-                categories: product.categories,
-                descriptions: siteData.descriptions,
-                uses: siteData.uses,
-                images: siteData.images,
-            };
-
-            res.status(200).json(response);
-        } catch (error) {
-            console.error("Error fetching product:", error);
-            res.status(500).json({ message: "Error fetching product", error });
         }
     },
 
@@ -117,12 +71,12 @@ const ProductController = {
                     { name: { $regex: searchTerm, $options: 'i' } },
                     { description: { $regex: searchTerm, $options: 'i' } }
                 ]
-            }).populate('category');
+            }).populate('categories');
             res.json(products);
         } catch (error) {
             res.status(500).json({ message: 'Error searching products', error });
         }
-    },
+    }
 };
 
 module.exports = ProductController;
