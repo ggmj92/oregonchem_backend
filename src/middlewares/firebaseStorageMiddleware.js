361e878aea13892561d88077d9817c39638e44dd
Gabriel Garcia-Miro Jeri <ggmj92@gmail.com>
2024-10-25 11:50:55 -0500

    upload fields corrected

--- src/middlewares/firebaseStorageMiddleware.js
+++ src/middlewares/firebaseStorageMiddleware.js
@@ -156,12 +156,6 @@ const upload = multer({
     limits: {
         fileSize: 1024 * 1024 * 3, // 3 MB limit
     },
-}).fields([
-    { name: 'images[site1]', maxCount: 1 },
-    { name: 'images[site2]', maxCount: 1 },
-    { name: 'images[site3]', maxCount: 1 },
-    { name: 'images[site4]', maxCount: 1 },
-    { name: 'images[site5]', maxCount: 1 },
-]);
+});
 
 module.exports = { upload, firebaseStorageMiddleware };
